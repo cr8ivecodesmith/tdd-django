19323054b96207b2ec07bc644d8b2ac00bb78648
Matt Lebrun <lebrun.matt@gmail.com>
2014-02-03 08:21:24 +0800

    Initial commit. Done chapter 1!

---
+++ superlists/functional_tests.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from pyvirtualdisplay import Display
+
+display = Display(visible=0, size=(1024, 768))
+display.start()
+
+browser = webdriver.Firefox()
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
+display.stop()
