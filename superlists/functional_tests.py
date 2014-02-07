2226efa82b4f0272edde707b2e9da2098e6918a2
Matt Lebrun <lebrun.matt@gmail.com>
2014-02-07 09:26:39 +0800

    Added ability to accept input in a simple way

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -7,16 +7,19 @@ from pyvirtualdisplay import Display
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
+        self.display = Display(visible=0, size=(1024, 768))
+        self.display.start()
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
+        self.display.stop()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith hears about a cool new online to-do app. She goes to check out its
         # homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get('http://192.168.56.32:8000')
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -41,15 +44,20 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            'New to-do item did not appear in the table.'
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item. She enters, "Use
         # peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # The page updates again and now shows both items in her list.
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',
+                      [row.text for row in rows])
 
         # Edith wonders whether the site will remember her list. The she sees that the
         # site generated a unique URL for her -- there is some explanatory text to that
@@ -61,7 +69,4 @@ class NewVisitorTest(unittest.TestCase):
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
-    display = Display(visible=0, size=(1024, 768))
-    display.start()
     unittest.main(warnings='ignore')
-    display.stop()
