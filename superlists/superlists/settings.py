f88edd31d1b437761f90556201e88524a8714216
Matt Lebrun <lebrun.matt@gmail.com>
2014-02-17 15:09:07 +0800

    Moved templates within app for best practice

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -81,8 +81,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'templates'),
-)
